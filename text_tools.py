423870d1a1c17a7987f065db42aed1bce79d3517
guillaume9320 <56391911+guillaume9320@users.noreply.github.com>
2020-05-30 21:16:26 +0200

    stats du jeu et police d'écriture texte

---
+++ text_tools.py
@@ -0,0 +1,27 @@
+import pygame
+
+
+# Thanks to https://stackoverflow.com/questions/42014195/rendering-text-with-multiple-lines-in-pygame/42015712
+def blit_text(surface, text, pos, font, color=pygame.Color('black')):
+    words = [word.split(' ') for word in text.splitlines()]  # 2D array where each row is a list of words.
+    space = font.size(' ')[0]  # The width of a space.
+    max_width, max_height = surface.get_size()
+    x, y = pos
+    for line in words:
+        for word in line:
+            word_surface = font.render(word, 0, color)
+            word_width, word_height = word_surface.get_size()
+            if x + word_width >= max_width:
+                x = pos[0]  # Reset the x.
+                y += word_height  # Start on new row.
+            surface.blit(word_surface, (x, y))
+            x += word_width + space
+        x = pos[0]  # Reset the x.
+        y += word_height  # Start on new row.
+
+
+def draw_text(text, font, color, surface, x, y):
+    textobj = font.render(text, 1, color)
+    textrect = textobj.get_rect()
+    textrect.topleft = (x, y)
+    surface.blit(textobj, textrect)
